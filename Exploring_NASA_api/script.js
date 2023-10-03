0ffce031a35addd9c58769bae097a2b3f5c1d042
Himadri Das <codingmaster40@gmail.com>
2023-10-03 23:48:16 +0530

    AJAX && creating elem using jquery

--- Exploring_NASA_api/script.js
+++ Exploring_NASA_api/script.js
@@ -1,9 +1,40 @@
-function displayImage() {
-    
+function displayImage(data) {
+
+    for(var ind in data) {
+        // slower method
+
+        // create an img element && add properties to it using jquery
+        // $('<img>', {
+        //     src: data[ind].url,
+        //     height: '100%',
+        //     width: '100%'
+        // }).appendTo('#image-container');
+
+        // faster method
+
+        var img = $(document.createElement('img'));
+        img.attr({
+            src: data[ind].url,
+        });
+        img.appendTo('#image-container');
+
+        // can also write the entire thing in 1 line
+    }
+
 }
 
 $.ajax({
-    url: 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY',
+    /* we can pass query parameters in the API only OR we can create a new key > data and via that 
+    pass query paramters */
+    url: 'https://api.nasa.gov/planetary/apod',
     method: 'GET',
-    success: displayImage
+    success: displayImage, // register a cb && not calling it > called if server returns some data(which is already in JSON format)
+    data: {
+        // passing query params > appended to the api
+        // for data range > it returns an array
+        api_key : 'DEMO_KEY',
+        // date: '2023-10-01'
+        start_date: '2023-10-01',
+        end_date: '2023-10-03'
+    }
 })
